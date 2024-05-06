614bf98aa5f5b6e3207a6aa0ec5db01fdbb7f7bc
Tim Velasquez <Timmyv1031@gmail.com>
2024-05-06 12:23:16 -0500

    Consolidated Frameworks
    At this time, I am working on CICD. I am wanting all code base in the same repo. Once I design RPA style, I will separate and add submodules (or DLL) library.

--- Playwright-XUnit/obj/Debug/net7.0/PlayWrightCSharp.AssemblyInfo.cs
+++ Playwright-XUnit/obj/Debug/net7.0/PlayWrightCSharp.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlayWrightCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3226a504208c5935ffad170b9e6d384e1b24d352")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+255543c8e984b3ddd0da688543cf9b3b0d3e6047")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlayWrightCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlayWrightCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
